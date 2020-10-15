c863f5df8c37da6ef39966146427a61854178231
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-15 15:07:46 +0300

    added test object

---
+++ src/main/java/org/application/models/TestObject.java
@@ -0,0 +1,15 @@
+package org.application.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestObject {
+
+    private String test = "testBack";
+}
--- src/main/java/org/application/resources/TestController.java
+++ src/main/java/org/application/resources/TestController.java
@@ -1,16 +1,21 @@
 package org.application.resources;
 
 
+import org.application.models.TestObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/v1/api/test")
 public class TestController {
 
+    private final static TestObject testObj = new TestObject();
+
     @GetMapping()
-    public String getTest(){
-        return "testBack";
+    public TestObject getTest(){
+        return testObj;
     }
 }
